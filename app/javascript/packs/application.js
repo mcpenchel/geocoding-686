6e6f59487a049d1c53e161a8f55329631d05dc1f
Matheus Penchel <matheus.penchel@gmail.com>
2021-10-29 19:46:43 -0300

    Finished implementing style + autocomplete

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -9,7 +9,9 @@ import 'mapbox-gl/dist/mapbox-gl.css';
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
+import { initAutocomplete } from '../plugins/init_autocomplete';
 
 document.addEventListener('turbolinks:load', () => {
   initMapbox();
+  initAutocomplete();
 });
--- app/javascript/plugins/init_mapbox.js
+++ app/javascript/plugins/init_mapbox.js
@@ -5,7 +5,7 @@ const buildMap = (mapElement) => {
   mapboxgl.accessToken = mapElement.dataset.mapboxApiKey;
   return new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/mapbox/streets-v10'
+    style: 'mapbox://styles/mcpenchel/ckelg5mu81aip19qm09g2h0zx'
   });
 };
 
